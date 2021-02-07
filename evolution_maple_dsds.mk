133d4b538ad86b52be138f75082af78c0af106bb
joker357 <42162775+joker357@users.noreply.github.com>
2021-02-08 01:11:58 +0800

    Update and rename lineage_maple_dsds.mk to evolution_maple_dsds.mk

--- evolution_maple_dsds.mk
+++ evolution_maple_dsds.mk
@@ -16,25 +16,19 @@ TARGET_SCREEN_WIDTH := 1080
 TARGET_BOOTANIMATION_HALF_RES := true
 
 ### LINEAGE
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_maple_dsds
+PRODUCT_NAME := evolution_maple_dsds
 PRODUCT_DEVICE := maple_dsds
 PRODUCT_BRAND := Sony
 PRODUCT_MODEL := G8142
 PRODUCT_MANUFACTURER := Sony
-
+TARGET_GAPPS_ARCH := arm64
+TARGET_BOOT_ANIMATION_RES := 1080
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=maple_dsds \
     PRIVATE_BUILD_DESC="G8142-user 9 47.2.A.10.107 172320177 release-keys"
 
 BUILD_FINGERPRINT := Sony/G8142/G8142:9/47.2.A.10.107/172320177:user/release-keys
 
-ifneq ($(LINEAGE_DEV_CERTIFICATE),)
-    PRODUCT_DEFAULT_DEV_CERTIFICATE := $(LINEAGE_DEV_CERTIFICATE)
-endif
-
-ifneq ($(LINEAGE_VERITY_CERTIFICATE),)
-    PRODUCT_VERITY_SIGNING_KEY := $(LINEAGE_VERITY_CERTIFICATE)
-endif
