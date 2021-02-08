a84ff4b2be28069bdff002405ec9d935298b86a7
joker357 <42162775+joker357@users.noreply.github.com>
2021-02-08 12:21:13 +0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -57,3 +57,6 @@ DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 ### SYSTEM PROPS
 # Add device-specific ones
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/vendor.prop
+### SElinux
+SELNUX_IGNORE_NEVERALLOWS := true
