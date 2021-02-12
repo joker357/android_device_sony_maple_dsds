35c00c68a9921e9d3fd2b337cffbc135d9d65de9
joker357 <42162775+joker357@users.noreply.github.com>
2021-02-12 20:09:43 +0800

    Update and rename fluid_maple_dsds.mk to nad_maple_dsds.mk

--- nad_maple_dsds.mk
+++ nad_maple_dsds.mk
@@ -16,10 +16,10 @@ TARGET_SCREEN_WIDTH := 1080
 TARGET_BOOTANIMATION_HALF_RES := true
 
 ### LINEAGE
-$(call inherit-product, vendor/fluid/config/common_full_phone.mk)
+$(call inherit-product, vendor/nad/config/common_full_phone.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := fluid_maple_dsds
+PRODUCT_NAME := nad_maple_dsds
 PRODUCT_DEVICE := maple_dsds
 PRODUCT_BRAND := Sony
 PRODUCT_MODEL := G8142
